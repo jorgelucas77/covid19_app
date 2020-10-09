1f8fb737d0c55f03adf3c9d8a6ae5f829d451a53
Jorge Lucas <jorgelucas77@gmail.com>
2020-10-09 16:24:24 +0200

    initial commit

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,49 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+import ChinaFlag from '../../assets/images/china.png'
+import GermanyFlag from '../../assets/images/germany.png'
+import ItalyFlag from '../../assets/images/italy.png'
+import UsaFlag from '../../assets/images/usa.png'
+import PortugalFlag from '../../assets/images/portugal.png'
+import MozambiqueFlag from '../../assets/images/mozambique.png'
+
+const COUNTRIES = [
+  {
+    label: 'Alemanha',
+    value: 'germany',
+    flag: GermanyFlag
+  },
+  {
+    label: 'Brasil',
+    value: 'brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'China',
+    value: 'china',
+    flag: ChinaFlag
+  },
+  {
+    label: 'Estados Unidos',
+    value: 'usa',
+    flag: UsaFlag
+  },
+  {
+    label: 'Itália',
+    value: 'italy',
+    flag: ItalyFlag
+  }
+  ,
+  {
+    label: 'Portugal',
+    value: 'portugal',
+    flag: PortugalFlag
+  }
+  ,
+  {
+    label: 'Mozambique',
+    value: 'mozambique',
+    flag: MozambiqueFlag
+  }
+]
+
+export default COUNTRIES
